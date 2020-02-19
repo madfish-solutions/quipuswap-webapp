7f9e67dc8f3e0d60377f4a19b692f3b46aee6ed8
Denis <1duglas1@gmail.com>
2020-02-19 16:16:37 +0200

    Send Form refactoring

--- src/views/Send.vue
+++ src/views/Send.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="max-w-xl mx-auto">
     <NavTabs class="mb-6" />
-    <div class="form-wrapper">
-      <FieldInput label="Input" placeholder="0.0" />
-      <div class="text-center py-2">
+    <Form>
+      <FormField label="Input" placeholder="0.0" />
+      <FormIcon>
         <img class="inline" src="@/assets/arrow-down.png" />
-      </div>
-      <FieldInput label="Output" placeholder="0.0" />
-      <div class="text-center py-2">
+      </FormIcon>
+      <FormField label="Output" placeholder="0.0" />
+      <FormIcon>
         <img class="inline" src="@/assets/arrow-down.png" />
-      </div>
-      <FieldInput label="Recipient Address" placeholder="0x1234..." :withSelect="false" />
-      <div class="py-2 px-6 flex justify-between">
+      </FormIcon>
+      <FormField label="Recipient Address" placeholder="0x1234..." :withSelect="false" />
+      <FormInfo class="flex justify-between">
         <span>Exchange rate</span>
         <span>-</span>
-      </div>
-    </div>
+      </FormInfo>
+    </Form>
 
     <div class="mx-auto text-center mt-8 mb-8 text-text text-sm font-normal">
       Select a token to continue
@@ -30,11 +30,12 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import FieldInput from "@/components/FieldInput.vue";
 import NavTabs from "@/components/NavTabs.vue";
 
+import Form, { FormIcon, FormField, FormInfo } from "@/components/Form";
+
 @Component({
-  components: { NavTabs, FieldInput },
+  components: { NavTabs, Form, FormIcon, FormField, FormInfo },
 })
 export default class Send extends Vue {}
 </script>
