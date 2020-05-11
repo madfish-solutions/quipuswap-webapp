646d77c2984090d9cf5b79e704f9f75906aad650
Alexandr Zosimchuk <al.zosimchuk@gmail.com>
2020-05-11 12:28:38 +0300

    add error handling

--- src/taquito/index.ts
+++ src/taquito/index.ts
@@ -5,6 +5,7 @@ const TEZOS_CONTRACT = "KT1R299cg7K94e3rFCGzpacTXJshQ43meffR";
 const tezos = new TezosToolkit();
 
 tezos.setProvider({ rpc: "https://api.tez.ie/rpc/carthagenet" });
+
 tezos.importKey(
   accountWithTokens.email,
   accountWithTokens.password,
@@ -16,11 +17,14 @@ export async function getAccountBalance(pkh: string) {
   const balance = await tezos.tz.getBalance(pkh);
   return balance;
 }
-
 export default async function useSmartContract() {
-  const pkhSrc = "tz1bwsEWCwSEXdRvnJxvegQZKeX5dj6oKEys";
-  const pkhDst = "tz1PirboZKFVqkfE45hVLpkpXaZtLk3mqC17";
-  const contract = await tezos.contract.at(TEZOS_CONTRACT);
-  const op = await contract.methods.transfer(pkhSrc, pkhDst, "10").send();
-  await op.confirmation();
+  try {
+    const pkhSrc = "tz1bwsEWCwSEXdRvnJxvegQZKeX5dj6oKEys";
+    const pkhDst = "tz1PirboZKFVqkfE45hVLpkpXaZtLk3mqC17";
+    const contract = await tezos.contract.at(TEZOS_CONTRACT);
+    const op = await contract.methods.transfer(pkhSrc, pkhDst, "1").send();
+    await op.confirmation();
+  } catch (e) {
+    console.log(e, "exception");
+  }
 }
