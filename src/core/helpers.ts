937b88125e5b6223ed3627c99e50cae74c1dd21e
serh11p <git@serh11p.com>
2021-03-09 12:17:22 +0200

    Implement duo connect with Temple

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -1,4 +1,5 @@
 import { validateAddress, ValidationResult } from "@taquito/utils";
+import { Signer } from "@taquito/taquito";
 import BigNumber from "bignumber.js";
 import { Tezos } from "./state";
 
@@ -44,4 +45,26 @@ export function snakeToCamel(str: string) {
       .replace("-", "")
       .replace("_", "")
   );
+}
+
+export class ReadOnlySigner implements Signer {
+  constructor(private pkh: string, private pk: string) {}
+
+  async publicKeyHash() {
+    return this.pkh;
+  }
+  async publicKey() {
+    return this.pk;
+  }
+  async secretKey(): Promise<string> {
+    throw new Error("Secret key cannot be exposed");
+  }
+  async sign(): Promise<{
+    bytes: string;
+    sig: string;
+    prefixSig: string;
+    sbytes: string;
+  }> {
+    throw new Error("Cannot sign");
+  }
 }
