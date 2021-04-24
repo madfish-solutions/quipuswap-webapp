1b767a5429f511b0f0421a06860e13d01cdd1296
serh11p <git@serh11p.com>
2021-04-24 16:32:24 +0300

    Maintanance

---
+++ src/views/UnderMaintenance.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-24 w-full flex flex-col items-center justify-center text-white text-xl text-center">
+   <div class="text-3xl mb-4">Application maintanance</div>
+   <div>Please, stay tuned. We are going to be back shortly</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class UnderMaintenance extends Vue {}
+</script>
