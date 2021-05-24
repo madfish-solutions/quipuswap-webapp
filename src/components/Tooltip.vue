7b3c699ebd3dbaf52ae9a7f4337ee1c5aa5e4472
olehkhalin <olehkhalin@gmail.com>
2021-05-24 17:03:22 +0300

    Created tooltips and disclaimer page

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    v-tooltip="{
+      content: content,
+      autoHide: false,
+      targetClasses: `tooltip-wrapper ${positions[position]} ${$attrs.parentclasses}`,
+      classes: 'tooltip-content',
+      delay: {
+        hide: 150,
+      },
+    }"
+  >
+    <img
+      :src="require('@/assets/tooltip-icon.svg')"
+      style="width: 20px; height: 20px;"
+    />
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import VTooltip from "v-tooltip";
+
+Vue.use(VTooltip);
+
+@Component
+export default class Tootip extends Vue {
+  @Prop() content?: HTMLElement | string;
+  @Prop({ default: "default" }) position?: string;
+  positions: object = {
+    default: "",
+    corner: "tooltip-corner",
+  };
+}
+</script>
+
+<style lang="postcss" scoped>
+.tooltip-wrapper {
+  padding: 5px;
+}
+.tooltip-wrapper img {
+  width: 20px;
+  height: 20px;
+}
+.tooltip-corner {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 0;
+}
+</style>
